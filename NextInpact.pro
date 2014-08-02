aa293ab5e0fe63a9a261fbc9ab5311b6427fac08
Guillaume Bour <guillaume@bour.cc>
2014-08-02 17:53:57 +0200

    [ui] ui mockup - 1st version

---
+++ NextInpact.pro
@@ -0,0 +1,38 @@
+# NOTICE:
+#
+# Application name defined in TARGET has a corresponding QML filename.
+# If name defined in TARGET is changed, the following needs to be done
+# to match new name:
+#   - corresponding QML filename must be changed
+#   - desktop icon filename must be changed
+#   - desktop filename must be changed
+#   - icon definition filename in desktop file must be changed
+#   - translation filenames have to be changed
+
+# The name of your application
+TARGET = NextInpact
+
+CONFIG += sailfishapp
+
+SOURCES += src/NextInpact.cpp
+
+OTHER_FILES += qml/NextInpact.qml \
+    qml/cover/CoverPage.qml \
+    rpm/NextInpact.changes.in \
+    rpm/NextInpact.spec \
+    rpm/NextInpact.yaml \
+    translations/*.ts \
+    NextInpact.desktop \
+    qml/pages/about.qml \
+    qml/pages/articles.qml \
+    qml/pages/detail.qml \
+    qml/pages/comments.qml
+
+# to disable building translations every time, comment out the
+# following CONFIG line
+CONFIG += sailfishapp_i18n
+TRANSLATIONS += translations/NextInpact-de.ts
+
+RESOURCES += \
+    resources.qrc
+
---
+++ translations/NextInpact.ts
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>My Cover</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SecondPage</name>
+    <message>
+        <source>Nested Page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Item</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>about</name>
+    <message>
+        <source>About ��NextINpact��</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>blablabla</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>articles</name>
+    <message>
+        <source>about</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
