ac8998ed98ae5189dd40dccda0e3e3d4764d38ea
Guillaume Bour <guillaume@bour.cc>
2014-08-31 21:00:36 +0200

    [feat] add french translation

--- translations/NextInpact-fr.ts
+++ translations/NextInpact-fr.ts
@@ -5,22 +5,23 @@
     <name>ArticleList</name>
     <message>
         <source>about</source>
-        <translation type="unfinished"></translation>
+        <translation>à propos</translation>
     </message>
     <message>
         <source>settings</source>
-        <translation type="unfinished"></translation>
+        <translation>paramètres</translation>
     </message>
     <message>
         <source>refresh</source>
-        <translation type="unfinished"></translation>
+        <translation>rafraîchir</translation>
     </message>
 </context>
+
 <context>
     <name>about</name>
     <message>
         <source>About ��NextINpact��</source>
-        <translation type="unfinished"></translation>
+        <translation>À propos de «NextINpact»</translation>
     </message>
     <message>
         <source>
@@ -30,7 +31,13 @@
                     &lt;br/&gt;&lt;br/&gt;
                     Articles and their contents displayed in this application; as NextINpact logo; are the entire property of &lt;b&gt;PC INpact SARL de presse&lt;/b&gt;.&lt;br/&gt;
                     If you like it, please &lt;a href=&quot;http://www.nextinpact.com/abonnement?utm_source=pcinpact&amp;utm_medium=header&amp;utm_campaign=premium&quot;&gt;subscribe to a member/premium account&lt;/a&gt;.</source>
-        <translation type="unfinished"></translation>
+        <translation>
+        L'application Jolla NextINpact est développée par &lt;a href=&quot;http://guillaume.bour.cc&quot;&gt;Guillaume Bour&lt;/a&gt;, &lt;/br&gt;
+        et distribuée selon les termes de la licence GPLv3.&lt;br/&gt;
+        Vous êtes encouragé à télécharger les sources sur &lt;a href=&quot;https://github.com/gbour/jolla-nextinpact&quot;&gt;github.com/gbour/jolla-nextinpact&lt;/a&gt; et à contribuer.
+        &lt;br/&gt;&lt;br/&gt;
+        Les articles et leur contenu affichés par cette application; ainsi que le logo NextINpact; sont la propriété pleine et entière de &lt;b&gt;PC INpact SARL de presse&lt;/b&gt;.&lt;br/&gt;
+        Si vous appréciez ce contenu, vous êtes invité à &lt;a href=&quot;http://www.nextinpact.com/abonnement?utm_source=pcinpact&amp;utm_medium=header&amp;utm_campaign=premium&quot;&gt;souscrire à un abonnement member ou premium&lt;/a&gt;.</translation>
     </message>
 </context>
 </TS>
