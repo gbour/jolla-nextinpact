6a516bb19df6246869bd89b7daeba017f43d3b4c
Guillaume Bour <guillaume@bour.cc>
2014-09-21 21:30:06 +0200

    [i18n] update fr translation

--- translations/NextInpact-fr.ts
+++ translations/NextInpact-fr.ts
@@ -18,14 +18,14 @@
                     Articles and their contents displayed in this application; as NextINpact logo; are the entire property of &lt;b&gt;PC INpact SARL de presse&lt;/b&gt;.&lt;br/&gt;
                     If you like it, please &lt;a href=&quot;http://www.nextinpact.com/abonnement?utm_source=pcinpact&amp;utm_medium=header&amp;utm_campaign=premium&quot;&gt;subscribe to a member/premium account&lt;/a&gt;.</source>
         <translation>
-                    version %1, publiée le %2 (git %3).
-                    &lt;br/&gt;&lt;br/&gt;
-                    L&apos;application Jolla NextINpact est développée par &lt;a href=&quot;http://guillaume.bour.cc&quot;&gt;Guillaume Bour&lt;/a&gt;, &lt;/br&gt;
-                    et distribuée selon les termes de la licence GPLv3.&lt;br/&gt;
-                    Vous êtes encouragé à télécharger les sources sur &lt;a href=&quot;https://github.com/gbour/jolla-nextinpact&quot;&gt;github.com/gbour/jolla-nextinpact&lt;/a&gt; et à contribuer.
-                    &lt;br/&gt;&lt;br/&gt;
-                    Les articles et leur contenu affichés par cette application; ainsi que le logo NextINpact; sont la propriété pleine et entière de &lt;b&gt;PC INpact SARL de presse&lt;/b&gt;.&lt;br/&gt;
-                    Si vous appréciez ce contenu, vous êtes invité à &lt;a href=&quot;http://www.nextinpact.com/abonnement?utm_source=pcinpact&amp;utm_medium=header&amp;utm_campaign=premium&quot;&gt;souscrire à un abonnement member ou premium&lt;/a&gt;.</translation>
+version %1, publiée le %2 (git %3).
+&lt;br/&gt;&lt;br/&gt;
+L&apos;application Jolla NextINpact est développée par &lt;a href=&quot;http://guillaume.bour.cc&quot;&gt;Guillaume Bour&lt;/a&gt;&lt;br/&gt;
+et distribuée selon les termes de la licence GPLv3.&lt;br/&gt;
+Vous êtes encouragé à télécharger les sources sur &lt;a href=&quot;https://github.com/gbour/jolla-nextinpact&quot;&gt;github.com/gbour/jolla-nextinpact&lt;/a&gt; et à contribuer.
+&lt;br/&gt;&lt;br/&gt;
+Les articles et leur contenu affichés par cette application; ainsi que le logo NextINpact; sont la propriété pleine et entière de &lt;b&gt;PC INpact SARL de presse&lt;/b&gt;&lt;br/&gt;.
+Si vous appréciez ce contenu, vous êtes invité à &lt;a href=&quot;http://www.nextinpact.com/abonnement?utm_source=pcinpact&amp;utm_medium=header&amp;utm_campaign=premium&quot;&gt;souscrire à un abonnement member ou premium&lt;/a&gt;.</translation>
     </message>
 </context>
 <context>
