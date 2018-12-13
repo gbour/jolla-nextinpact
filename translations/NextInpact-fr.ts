e6801b19fd8994ea57e9cb52a7092e403405081c
Guillaume Bour <guillaume@bour.cc>
2018-12-13 20:46:02 +0100

    [i18n] update translations

--- translations/NextInpact-fr.ts
+++ translations/NextInpact-fr.ts
@@ -4,8 +4,12 @@
 <context>
     <name>About</name>
     <message>
+        <source>About «NextINpact»</source>
+        <translation>À propos de «NextInpact»</translation>
+    </message>
+    <message>
         <source>
-                    version %1, released at %2 (git %3).&lt;br/&gt;
+                    version %1, released on %2.&lt;br/&gt;
                     &lt;br/&gt;
                     The NextINpact Jolla application is developped by &lt;a href=&quot;http://guillaume.bour.cc&quot;&gt;Guillaume Bour&lt;/a&gt;, &lt;/br&gt;
                     and distributed under GPLv3 license.&lt;br/&gt;
@@ -14,18 +18,15 @@
                     Articles and their contents displayed in this application; as NextINpact logo; are the entire property of &lt;b&gt;PC INpact SARL de presse&lt;/b&gt;.&lt;br/&gt;
                     If you like it, please &lt;a href=&quot;http://www.nextinpact.com/abonnement?utm_source=pcinpact&amp;utm_medium=header&amp;utm_campaign=premium&quot;&gt;subscribe to a member/premium account&lt;/a&gt;.</source>
         <translation>
-version %1, publiée le %2 (git %3).
-&lt;br/&gt;&lt;br/&gt;
-L&apos;application Jolla NextINpact est développée par &lt;a href=&quot;http://guillaume.bour.cc&quot;&gt;Guillaume Bour&lt;/a&gt;&lt;br/&gt;
-et distribuée selon les termes de la licence GPLv3.&lt;br/&gt;
-Vous êtes encouragé à télécharger les sources sur &lt;a href=&quot;https://github.com/gbour/jolla-nextinpact&quot;&gt;github.com/gbour/jolla-nextinpact&lt;/a&gt; et à contribuer.
-&lt;br/&gt;&lt;br/&gt;
-Les articles et leur contenu affichés par cette application; ainsi que le logo NextINpact; sont la propriété pleine et entière de &lt;b&gt;PC INpact SARL de presse&lt;/b&gt;&lt;br/&gt;.
-Si vous appréciez ce contenu, vous êtes invité à &lt;a href=&quot;http://www.nextinpact.com/abonnement?utm_source=pcinpact&amp;utm_medium=header&amp;utm_campaign=premium&quot;&gt;souscrire à un abonnement member ou premium&lt;/a&gt;.</translation>
-    </message>
-    <message>
-        <source>About «NextINpact»</source>
-        <translation type="unfinished"></translation>
+                    version %1, publiée le %2.&lt;br/&gt;
+                    &lt;br/&gt;
+                    L&apos;application Jolla NextINpact est développée par &lt;a href=&quot;http://guillaume.bour.cc&quot;&gt;Guillaume Bour&lt;/a&gt;&lt;br/&gt;
+                    et distribuée selon les termes de la licence GPLv3.&lt;br/&gt;
+                    Vous êtes encouragé à télécharger les sources sur &lt;a href=&quot;https://github.com/gbour/jolla-nextinpact&quot;&gt;github.com/gbour/jolla-nextinpact&lt;/a&gt; et à contribuer.
+                    &lt;br/&gt;&lt;br/&gt;
+                    Les articles et leur contenu affichés par cette application; ainsi que le logo NextINpact; sont la propriété pleine et entière de &lt;b&gt;PC INpact SARL de presse&lt;/b&gt;&lt;br/&gt;.
+                    Si vous appréciez ce contenu, vous êtes invité à &lt;a href=&quot;http://www.nextinpact.com/abonnement?utm_source=pcinpact&amp;utm_medium=header&amp;utm_campaign=premium&quot;&gt;souscrire à un abonnement member ou premium&lt;/a&gt;.</translation>
+        </message>
     </message>
 </context>
 <context>
--- translations/NextInpact.ts
+++ translations/NextInpact.ts
@@ -4,8 +4,12 @@
 <context>
     <name>About</name>
     <message>
+        <source>About «NextINpact»</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <source>
-                    version %1, released at %2 (git %3).&lt;br/&gt;
+                    version %1, released on %2.&lt;br/&gt;
                     &lt;br/&gt;
                     The NextINpact Jolla application is developped by &lt;a href=&quot;http://guillaume.bour.cc&quot;&gt;Guillaume Bour&lt;/a&gt;, &lt;/br&gt;
                     and distributed under GPLv3 license.&lt;br/&gt;
@@ -13,10 +17,6 @@
                     &lt;br/&gt;&lt;br/&gt;
                     Articles and their contents displayed in this application; as NextINpact logo; are the entire property of &lt;b&gt;PC INpact SARL de presse&lt;/b&gt;.&lt;br/&gt;
                     If you like it, please &lt;a href=&quot;http://www.nextinpact.com/abonnement?utm_source=pcinpact&amp;utm_medium=header&amp;utm_campaign=premium&quot;&gt;subscribe to a member/premium account&lt;/a&gt;.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>About «NextINpact»</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
